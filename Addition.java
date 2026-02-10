1bb558c773f1d60fc3f5c2b4145bc24b3ded3177
kirisvesit <2025.kirisraja.navidhar@ves.ac.in>
2026-02-10 14:51:29 +0530

    Implement Addition class to sum two integers

---
+++ Addition.java
@@ -0,0 +1,9 @@
+class Addition{
+  public static void main(String[] args){
+    System.out.println("Addition of two " );
+     int a = Integer.parseInt(args[0]);
+     int b = Integer.parseInt(args[1]);
+     System.out.println("Sum = " + (a + b));
+
+  }
+}
